33b878a9b85b008222f559cbda936ab3e759c198
duncanidaho14 <elhadibeddarem@gmail.com>
2020-10-11 19:02:31 +0200

    correction de bug, css, gestion des erreurs

---
+++ .history/src/DataFixtures/AppFixtures_20201011004847.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Role;
+use App\Entity\User;
+use App\Entity\Image;
+use App\Entity\Annonce;
+use App\Entity\Booking;
+use App\Entity\Comment;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('Fr-fr');
+
+        $adminRole = new Role();
+        $adminRole->setTitle('ROLE_ADMIN');
+        $manager->persist($adminRole);
+
+        $adminUser = new User();
+        $adminUser->setFirstName('kirua')
+                    ->setLastName('zoldik')
+                    ->setEmail('kirua@gmail.com')
+                    ->setHash($this->encoder->encodePassword($adminUser, 'password'))
+                    ->setPicture('https://www.google.com/search?q=kirua+image&tbm=isch&source=iu&ictx=1&fir=Yz_rLakGypnqjM%252Ce8-bc8x2nRGw2M%252C_&vet=1&usg=AI4_-kThIWgK0TcpMBjJoU-bkPLb6ppjtA&sa=X&ved=2ahUKEwj83d7p96nsAhUQ-hQKHdH-D6YQ9QF6BAgKEEQ&biw=874&bih=798#imgrc=Yz_rLakGypnqjM')
+                    ->setIntroduction($faker->sentence())
+                    ->setDescription('<p>'.join('</p><p>', $faker->paragraphs(3)) .'</p>')
+                    ->addUserRole($adminRole);
+        $manager->persist($adminUser);
+
+        //Nous gérons les utilisateurs
+        $users = [];
+        $genres = ['male', 'female'];
+
+        for ($i=0; $i <= 10; $i++) { 
+            $user = new User();
+
+            $genre = $faker->randomElement($genres);
+
+            $picture = 'https://randomuser.me/api/portraits/';
+            $pictureId = $faker->numberBetween(1, 99) . '.jpg';
+
+            $picture .= ($genre == 'male' ? 'men/' : 'women/'). $pictureId;
+
+            $hash = $this->encoder->encodePassword($user, 'password');
+
+            $user->setFirstName($faker->firstname($genre))
+                ->setLastName($faker->lastname)
+                ->setEmail($faker->email)
+                ->setIntroduction($faker->sentence())
+                ->setDescription('<p>'.join('<p></p>', $faker->paragraphs(3)). '</p>')
+                ->setHash($hash)
+                ->setPicture($picture)
+            ;
+            
+            $manager->persist($user);
+            $users[] = $user;
+        }
+        //Nous gérons les annonces
+        for ($i=1; $i < 30; $i++) { 
+            $annonce = new Annonce();
+
+            
+            $title = $faker->sentence();
+            $coverImage = $faker->imageUrl(500, 500);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>'.join('</p><p>', $faker->paragraphs(5)) . '</p>';
+
+
+            $user = $users[mt_rand(0, count($users) -1)];
+
+            $annonce->setTitle($title)
+                ->setCoverImage($coverImage)
+                ->setIntroduction($introduction)
+                ->setContent($content)
+                ->setPrice(mt_rand(40, 200))
+                ->setRooms(mt_rand(1, 5))
+                ->setAuthor($user)
+                ->setStreet($faker->streetAddress)
+                ->setZip($faker->postcode)
+                ->setLat($faker->latitude())
+                ->setLon($faker->longitude)
+                ;
+            for ($j=0; $j <= mt_rand(2, 5) ; $j++) { 
+                $image = new Image();
+                $image->setUrl($faker->imageUrl())
+                      ->setCaption($faker->sentence())
+                      ->setAnnonce($annonce);
+
+                $manager->persist($image);
+            }
+
+            //Gestion des réservations
+            for ($j=0; $j <= \mt_rand(0, 10) ; $j++) { 
+                $booking = new Booking();
+
+                $createdAt = $faker->dateTimeBetween('-6 months');
+                $startDate = $faker->dateTimeBetween('-3 months');
+                // Gestion de la date de fin
+                $duration = mt_rand(3, 10);
+
+                // clone la startDate pour la modifier
+                $endDate = (clone $startDate)->modify("+$duration days");
+                $amount = $annonce->getPrice() * $duration;
+
+                $booker = $users[mt_rand(0, count($users) -1)];
+                
+                $comment = $faker->paragraph();
+
+                $booking->setBooker($booker)
+                        ->setAnnonce($annonce)
+                        ->setStartDate($startDate)
+                        ->setEndDate($endDate)
+                        ->setCreatedAt($createdAt)
+                        ->setAmount($amount)
+                        ->setComment($comment)
+                ;
+                
+                $manager->persist($booking);
+
+                // Gestion des commentaires
+                if (mt_rand(0, 1)) {
+                    $comment = new Comment();
+                    $comment->setContent($faker->paragraph())
+                            ->setRating(mt_rand(1, 5))
+                            ->setAuthor($booker)
+                            ->setAnnonce($annonce);
+                    
+                    $manager->persist($comment);
+                }
+            }
+            $manager->persist($annonce);
+        }
+        
+
+        $manager->flush();
+    }
+}
---
+++ .history/src/Entity/Annonce_20201010135510.php
@@ -0,0 +1,438 @@
+<?php
+
+namespace App\Entity;
+
+
+use DateTime;
+use App\Entity\User;
+use Cocur\Slugify\Slugify;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\AnnonceRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=AnnonceRepository::class)
+ * @ORM\HasLifecycleCallbacks
+ * @UniqueEntity(
+ *  fields={"title"},
+ *  message="Une autre annonce possède déjà ce titre, merci de le modifier"
+ * )
+ */
+class Annonce
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *  @Assert\Length(min=10, max=255, minMessage="Le titre doit faire plus de 10 caractères !", maxMessage="Le titre ne peut pas faire plus de 255 caractères")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\Length(min=20, minMessage="Votre introduction doit faire plus de 20 caractères !")
+     */
+    private $introduction;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\Length(min=100, minMessage="Votre description doit faire plus de 100 caractères !")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Url()
+     */
+    private $coverImage;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $rooms;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Image::class, mappedBy="annonce", orphanRemoval=true)
+     * @Assert\Valid()
+     */
+    private $images;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="annonces")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Booking::class, mappedBy="annonce")
+     */
+    private $bookings;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="annonce", orphanRemoval=true)
+     */
+    private $comments;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $zip;
+
+    /**
+     * @ORM\Column(type="decimal", scale=6, precision=8)
+     */
+    private $lat;
+
+    /**
+     * @ORM\Column(type="decimal", scale=6, precision=8)
+     */
+    private $lon;
+
+    public function __construct()
+    {
+        $this->images = new ArrayCollection();
+        $this->bookings = new ArrayCollection();
+        $this->comments = new ArrayCollection();
+    }
+
+    /**
+    * Permet d'initialiser le slug
+    * @ORM\PrePersist
+    * @ORM\PreUpdate
+    * @return void
+    */
+    public function initializeSlug()
+    {
+        if (empty($this->slug)) {
+            $slugify = new Slugify();
+            $this->slug = $slugify->slugify($this->title);
+        }
+    }
+
+    /**
+     * Permet de récupérer le commentaire d'un auteur par rapport à une annonce
+     * 
+     * 
+     *
+     * @param User $author
+     * @return Comment|null
+     */
+    public function getCommentFromAuthor(User $author)
+    {
+        foreach ($this->comments as $comment) {
+            if ($comment->getAuthor() === $author) {
+                return $comment;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Permet d'obtenir la moyenne globale des notes pour cette annonce
+     *
+     * @return float
+     */
+    public function getAvgRatings()
+    {
+        //Calculer la somme des notes
+        // la fonction array_reduce () envoie les valeurs d'un tableau à une fonction définie par l'utilisateur et renvoie une chaîne.
+        $sum = \array_reduce($this->comments->toArray(), function($total, $comment){
+            return $total + $comment->getRating();
+        }, 0);
+
+        // Faire la division pour avoir la moyenne
+        if(count($this->comments) > 0) return $sum / count($this->comments);
+
+        return 0;
+    }
+
+    /**
+     * Permet d'obtenir un tableau des jours qui ne sont pas disponibles pour cette annonce
+     *
+     * @return array Un tableau d'objets Datetime représentant les jours d'occupation
+     */
+    public function getNotAvailableDays()
+    {
+        $notAvailableDays = [];
+
+        foreach ($this->bookings as $booking) {
+            // Calculer les jours qui se trouvent entre la date d'arrivée et de départ
+            $resultat = range($booking->getStartDate()->getTimestamp(),
+                                $booking->getEndDate()->getTimestamp(),
+                                24 * 60 * 60
+            );
+
+            $days = array_map(function($dayTimestamp) {
+                return new \DateTime(date('Y-m-d', $dayTimestamp));
+            }, $resultat);
+
+            $notAvailableDays = array_merge($notAvailableDays, $days);
+        }
+        return $notAvailableDays;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getIntroduction(): ?string
+    {
+        return $this->introduction;
+    }
+
+    public function setIntroduction(string $introduction): self
+    {
+        $this->introduction = $introduction;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCoverImage(): ?string
+    {
+        return $this->coverImage;
+    }
+
+    public function setCoverImage(string $coverImage): self
+    {
+        $this->coverImage = $coverImage;
+
+        return $this;
+    }
+
+    public function getRooms(): ?int
+    {
+        return $this->rooms;
+    }
+
+    public function setRooms(int $rooms): self
+    {
+        $this->rooms = $rooms;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Image[]
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): self
+    {
+        if (!$this->images->contains($image)) {
+            $this->images[] = $image;
+            $image->setAnnonce($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): self
+    {
+        if ($this->images->contains($image)) {
+            $this->images->removeElement($image);
+            // set the owning side to null (unless already changed)
+            if ($image->getAnnonce() === $this) {
+                $image->setAnnonce(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Booking[]
+     */
+    public function getBookings(): Collection
+    {
+        return $this->bookings;
+    }
+
+    public function addBooking(Booking $booking): self
+    {
+        if (!$this->bookings->contains($booking)) {
+            $this->bookings[] = $booking;
+            $booking->setAnnonce($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBooking(Booking $booking): self
+    {
+        if ($this->bookings->contains($booking)) {
+            $this->bookings->removeElement($booking);
+            // set the owning side to null (unless already changed)
+            if ($booking->getAnnonce() === $this) {
+                $booking->setAnnonce(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setAnnonce($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getAnnonce() === $this) {
+                $comment->setAnnonce(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getZip(): ?string
+    {
+        return $this->zip;
+    }
+
+    public function setZip(string $zip): self
+    {
+        $this->zip = $zip;
+
+        return $this;
+    }
+
+    public function getLat(): ?float
+    {
+        return $this->lat;
+    }
+
+    public function setLat(float $lat): self
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    public function getLon(): ?float
+    {
+        return $this->lon;
+    }
+
+    public function setLon(float $lon): self
+    {
+        $this->lon = $lon;
+
+        return $this;
+    }
+}
