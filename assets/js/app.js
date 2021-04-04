d2775d2eb2584103c1fc7d5cdb39449a40b8facc
duncanidaho14 <elhadibeddarem@gmail.com>
2021-04-05 00:40:29 +0200

    add leaftmap

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.scss';
-import '../css/_variables.scss';
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
 global.$ = global.jQuery = $;
